9be527233b057e9fb716b0b89db9b159192e1a02
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-06-04 20:53:26 -0300

    UPLOADS: Adicionado imagens e ajustado rota para acessar as imagens

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express, { response, json } from 'express';
+import path from 'path';
 import routes from './routes';
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.listen(3333);
